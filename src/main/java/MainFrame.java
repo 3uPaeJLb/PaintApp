ccc45e5a01af3713b4b693b3162c10bbf2dc8e6a
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2025-10-01 22:28:31 +0700

    Made draw field

---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    private DrawField drawField;
+
+    public MainFrame()
+    {
+        super("Paint");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(400, 100, 640, 480);
+        setMinimumSize(new Dimension(640, 480));
+
+        drawField = new DrawField();
+        add(drawField);
+    }
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
